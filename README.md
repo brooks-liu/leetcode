cbe102747c9bbc458393c79caeca72dfad8c594d
Brooks Liu <lcxbrooks@gmail.com>
2024-07-22 14:37:06 -0600

    uploaded solution to question 2, add two numbers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # leetcode
- A collection of my solutions to LeetCode problems, to serve as a progress tracker.
+
+A collection of my solutions to LeetCode problems, to serve as a progress tracker.
+The first few uploads are a few that I have already done, so they likely have not been commented much.
+There may be multiple solutions in each file from me wanting to improve my solution.
---
+++ medium-add_two_numbers.py
@@ -0,0 +1,107 @@
+#Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# solution 1 ----------------------------------------------------
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = ListNode(0, None)
+        cur = res
+
+        while l1 != None and l2 != None:
+            if l1.val + l2.val + cur.val > 9:
+                cur.next = ListNode(1, None)
+                cur.val = l1.val + l2.val + cur.val - 10
+            else:
+                cur.next = ListNode(0, None)
+                cur.val = l1.val + l2.val + cur.val
+            cur = cur.next
+            l1 = l1.next
+            l2 = l2.next
+
+        #now at least one of l1 and l2 is empty
+
+        if l1 == None:
+            while l2 != None:
+                if l2.val + cur.val > 9:
+                    cur.next = ListNode(1, None)
+                    cur.val = l2.val + cur.val - 10
+                else:
+                    cur.next = ListNode(0, None)
+                    cur.val = l2.val + cur.val
+                l2 = l2.next
+                cur = cur.next
+        else:
+            while l1 != None:
+                if l1.val + cur.val > 9:
+                    cur.next = ListNode(1, None)
+                    cur.val = l1.val + cur.val - 10
+                else:
+                    cur.next = ListNode(0, None)
+                    cur.val = l1.val + cur.val
+                l1 = l1.next
+                cur = cur.next
+        
+        #final check, remove last node
+        temp = res
+        if cur.val == 0:
+            while temp.next != None and temp.next.next != None:
+                temp = temp.next
+            temp.next = None
+
+        return res
+                
+
+# solution 2 ----------------------------------------------------
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = []
+        carry = 0
+        
+        while l1 is not None or l2 is not None:
+            num1 = l1.val if l1 is not None else 0
+            num2 = l2.val if l2 is not None else 0
+            sum = num1 + num2 + carry
+            carry = sum // 10
+            sum = sum % 10
+            res.append(sum)
+            if l1 != None:
+                l1 = l1.next 
+            if l2 != None:
+                l2 = l2.next
+
+        if carry != 0:
+            res.append(carry)
+
+        SolutionHead = ListNode(0, None)
+        tail = SolutionHead
+        tail.val = res[0]
+        
+        for i in range(1, len(res)):
+            tail.next = ListNode(res[i], None)
+            tail = tail.next
+
+        return SolutionHead
+
+
+
+        
+
+
+        
+
+
+        
