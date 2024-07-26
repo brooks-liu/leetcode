9a7d7ea9051f957d49df132a01fee21cf797ce7b
Brooks Liu <149959401+brooks-liu@users.noreply.github.com>
2024-07-26 14:58:38 -0600

    uploaded problem 21, merge two sorted lists
    it went from beating 4% to 70% (and i did the solution in one try!!)

---
+++ easy-merge_two_sorted_lists.py
@@ -0,0 +1,53 @@
+# beats 73.91% of solutions
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# solution 1-----------------------------------------------------------------
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if list1 == None and list2 == None:
+            return None
+        res = ListNode(0, None)
+        cur = res
+        while list1 != None and list2 != None:
+            if list1.val < list2.val:
+                cur.val = list1.val
+                list1 = list1.next
+            else:
+                cur.val = list2.val
+                list2 = list2.next
+            if list1 != None or list2 != None:
+                cur.next = ListNode(0, None)
+                cur = cur.next
+
+        if list1 == None and list2 == None:
+            return res
+        if list1 == None:
+            while list2 != None:
+                cur.val = list2.val
+                list2 = list2.next
+                if list2 != None:
+                    cur.next = ListNode(0, None)
+                    cur = cur.next
+        if list2 == None:
+            while list1 != None:
+                cur.val = list1.val
+                list1 = list1.next
+                if list1 != None:
+                    cur.next = ListNode(0, None)
+                    cur = cur.next
+        return res
+
+
+
+        
