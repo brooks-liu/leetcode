ecded39739bf028ae19469f5d580a46924f3f6a0
Brooks Liu <149959401+brooks-liu@users.noreply.github.com>
2024-08-12 21:19:40 -0600

    Updated problem 41, first missing positive

--- hard-first_missing_positive.py
+++ hard-first_missing_positive.py
@@ -1,5 +1,7 @@
-# beats 65.75% of solutions
+# beats 66.88% of solutions (solution 1), not the most space efficient so im trying to make that better
+# solution 2 is much more space efficient, better than 95.82% of solutions
 
+# solution 1----------------------------------------
 class Solution(object):
     def firstMissingPositive(self, nums):
         """
@@ -21,17 +23,31 @@ class Solution(object):
         for i in range(length):
             if nums[i] in numsLeft:
                 numsLeft[nums[i]] = 0
-            else:
-                continue
-        
+
         for i in range(1, length + 2):
             if numsLeft[i] == 1:
                 return i
-            
 
 
+# solution 2---------------------------------------
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        i = 0
+        while i < n:
+            spot = nums[i] - 1
+            if 0 < nums[i] <= n and nums[i] != nums[spot]:
+                nums[i], nums[spot] = nums[spot], nums[i]
+            else:
+                i += 1
         
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
 
-        
-        
+        return n + 1
         
