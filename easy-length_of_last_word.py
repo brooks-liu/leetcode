f3b00d487d67aa0ea9cc4fb000f0576efd647e1c
Brooks Liu <149959401+brooks-liu@users.noreply.github.com>
2024-08-15 14:55:07 -0600

    problem 58, length of last word

---
+++ easy-length_of_last_word.py
@@ -0,0 +1,12 @@
+# beast 90.60% of solutions
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split()
+        return len(words[-1])
+        
+
