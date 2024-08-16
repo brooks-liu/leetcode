04a82a50497e9709fc43baafc5c93fe9508d1c3b
Brooks Liu <149959401+brooks-liu@users.noreply.github.com>
2024-08-15 21:57:13 -0600

    Update easy-length_of_last_word.py

--- easy-length_of_last_word.py
+++ easy-length_of_last_word.py
@@ -1,4 +1,6 @@
-# beast 90.60% of solutions
+# beats 90.60% of solutions (both solutions ran 90.60%)
+
+#solution 1---------------------------------
 
 class Solution(object):
     def lengthOfLastWord(self, s):
@@ -10,3 +12,23 @@ class Solution(object):
         return len(words[-1])
         
 
+#solution 2---------------------------------
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        if len(s) == 0:
+            return count
+        for i in range(len(s) - 1, 0, -1):
+            if s[i] == ' ' and count != 0:
+                return count
+            elif s[i] != ' ':
+                count += 1
+        
+        if s[0] != ' ':
+            return count + 1
+        
+        return count
